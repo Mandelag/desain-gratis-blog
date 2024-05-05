4314ba584b637b1ee9706408d8d536bcc94fe7fd
keenan.gebze <keenan.gebze@gmail.com>
2024-05-05 22:59:30 +0700

    blogspot add

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,23 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+import Link from "next/link";
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
-}
+  title: "Blog",
+  description: "Read my blog.",
+};
 
 export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
       <BlogPosts />
+      <br />
+      <p className="text mb-8 tracking-tighter">
+        My{" "}
+        <Link href="https://mandelag.blogspot.com/" target="_blank">
+          legacy blogspot
+        </Link>
+      </p>
     </section>
-  )
+  );
 }
