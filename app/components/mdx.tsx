c555ff161aef50dd91c3845ad4c5f67ae23ca525
Keenan Gebze <keenan.gebze@gmail.com>
2025-01-28 17:26:43 +0700

    style update + ramblings

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -10,28 +10,90 @@ import Markdown from "react-markdown";
 import supersub from "remark-supersub";
 
 const tableStyles = `
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      margin-bottom: 2em;
-    }
-    th {
-      border-bottom: 1px solid #d0d0d0;
-      font-weight: 400;
-      text-align: left;
-      padding-left: 24px;
-      padding-right: 24px;
-      background-color: #eaeaea;
-      height: 3em;
-    }
-    tr:nth-child(odd) {
-      background-color: #fdfdfd;
-    }
-    td {
-      border-bottom: 1px solid #eaeaea;
-      font-family: Monospace;
-      height: 3em;
-    }
+// https://github.com/micromark/micromark-extension-gfm-table?tab=readme-ov-file#css
+// https://github.com/micromark/micromark-extension-gfm-table#css
+// https://github.com/remarkjs/react-markdown/issues/493
+/* Light theme. */
+:root {
+  --color-canvas-default: #ffffff;
+  --color-canvas-subtle: #f6f8fa;
+  --color-border-default: #d0d7de;
+  --color-border-muted: hsla(210, 18%, 87%, 1);
+}
+
+/* Dark theme. */
+@media (prefers-color-scheme: dark) {
+  :root {
+    --color-canvas-default: #0d1117;
+    --color-canvas-subtle: #161b22;
+    --color-border-default: #30363d;
+    --color-border-muted: #21262d;
+  }
+}
+
+
+table {
+  border-spacing: 0;
+  border-collapse: collapse;
+  display: block;
+  // margin-top: 0;
+  margin-bottom: 16px;
+  width: max-content;
+  max-width: 100%;
+  overflow: auto;
+  margin: 8px;
+  font-size: 0.9em;
+  margin-top: 1em;
+}
+
+tr {
+  background-color: var(--color-canvas-default);
+  border-top: 1px solid var(--color-border-muted);
+}
+
+tr:nth-child(2n) {
+  background-color: var(--color-canvas-subtle);
+}
+
+td,
+th {
+  padding: 6px 13px;
+  border: 1px solid var(--color-border-default);
+}
+
+th {
+  font-weight: 600;
+}
+
+table img {
+  background-color: transparent;
+}
+
+
+    // table {
+    // margin: 1em;
+    //   // width: 100%;
+    //   // border-collapse: collapse;
+    //   margin-bottom: 2em;
+    //   border: 1px solid white;
+    // }
+    // th {
+    //   // border-bottom: 1px solid #d0d0d0;
+    //   font-weight: 400;
+    //   text-align: left;
+    //   padding-left: 24px;
+    //   padding-right: 24px;
+    //   background-color: #eaeaea33;
+    //   height: 3em;
+    // }
+    // tr:nth-child(odd) {
+    //   // background-color: #fdfdfd;
+    // }
+    // td {
+    //   border-bottom: 1px solid #eaeaea;
+    //   // font-family: Monospace;
+    //   height: 3em;
+    // }
 
   `;
 
