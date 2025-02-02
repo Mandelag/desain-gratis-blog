be084a2992a3e34f2a32e03ad67c9fa5ed962730
Keenan Gebze <keenan.gebze@gmail.com>
2025-02-03 02:11:12 +0700

    working renderings

--- app/waiting-room/page.tsx
+++ app/waiting-room/page.tsx
@@ -1,50 +1,51 @@
 'use client'
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Mesh } from 'three'
+import { OrbitControls } from '@react-three/drei';
 
-function Box(props) {
 
-  // This reference will give us direct access to the mesh
+export default function App() {
+
+  // <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
+  return <div>
+    <div style={{ width: "100%", height: "500px" }}>
+      <Canvas >
+        <ambientLight intensity={Math.PI / 8} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={2} intensity={Math.PI} />
+        <Boxx />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  </div>
+}
+function Boxx(
+
+) {
   const meshRef = useRef<Mesh>(null!)
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  const [active, setActive] = useState(false);
+  const [hover, setHover] = useState(false);
 
 
-  return <mesh
-    {...props}
-    ref={meshRef}
-    scale={active ? 1.5 : 1}
-    onClick={(event) => setActive(!active)}
-    onPointerOver={(event) => setHover(true)}
-    onPointerOut={(event) => setHover(false)}>
-    <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-  </mesh>
+  useEffect(() => {
+    if (!hover) {
+      return
+    }
+    console.log("HOVERFED!")
+  }, [hover])
+
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta / 8))
+  return <>
+    <mesh
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+      ref={meshRef}
+    >
+      <sphereGeometry />
+      <meshStandardMaterial color={hover ? 'green' : 'orange'} roughness={0.0} wireframe />
+    </mesh>
+  </>
 }
-
-export default function WRCanvas() {
-  return (
-    <Canvas style={{
-      // position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '500px',
-      pointerEvents: 'none',
-    }}>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      {/* <OrbitControls /> */}
-    </Canvas>
-  )
-}
-
